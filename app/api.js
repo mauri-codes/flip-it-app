66e48bd18efee2a32d772fce6e9a7b07e15514a8
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-07-03 00:05:33 -0400

    created bare-bones sam API, works locally

---
+++ app/api.js
@@ -0,0 +1,22 @@
+// const axios = require('axios')
+let response;
+
+exports.get_collections = async (event, context) => {
+    try {
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify([{
+                collection_id: 1,
+                name: "science"
+            },{
+                collection_id: 2,
+                name: "medicine"
+            }])
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
