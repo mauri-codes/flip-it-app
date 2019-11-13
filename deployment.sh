2d776c7d8ac5563275a2ec1a13d6c9899e52dfac
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-11-12 22:27:12 -0400

    included cognito authorizer

--- deployment.sh
+++ deployment.sh
@@ -5,6 +5,7 @@ export DEPLOYMENTS_BUCKET=mauri-deployments
 export DOMAIN_NAME="api.flip.mauridev.net"
 export CERTIFICATE="arn:aws:acm:us-east-1:972073858291:certificate/bdf31f78-5e34-4bf7-a098-075b39f205ec"
 export HOSTED_ZONE_ID="/hostedzone/Z1IUAUSKRI6O7T"
+export USER_POOL="arn:aws:cognito-idp:us-east-2:972073858291:userpool/us-east-2_RgHEA6K8Z"
 
 
 if [ $1 = "-c" ] || [ $1 = "--create" ]
@@ -38,6 +39,7 @@ then
     --stack-name flip \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
+      UserPool=$USER_POOL \
       StageName=$STAGE_NAME \
       DomainName=$DOMAIN_NAME \
       Certificate=$CERTIFICATE \
